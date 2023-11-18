62c88784d8f062ec8a1c9399a3a1f77307bb1d2e
john-dev <me@slev.de>
2023-11-18 12:46:56 +0100

    Add: initial layout

---
+++ layouts/default.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="w-screen h-screen bg-gray-900 text-gray-300">
+    <div class="w-screen h-screen overflow-hidden flex flex-row font-[Poppins]">
+      <NuxtPage />
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup></script>
