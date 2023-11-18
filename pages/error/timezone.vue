28379c15fdcaec72d6dccec019797309ed1f6cd5
john-dev <me@slev.de>
2023-11-18 12:49:00 +0100

    Add: error page for invalid timezone

---
+++ pages/error/timezone.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="m-auto">
+    invalid timezone
+    <span
+      v-if="route.query['timezone'] && route.query['timezone'] !== ''"
+      class="-ml-1"
+      >: <span class="text-red-500">{{ route.query["timezone"] }}</span></span
+    >
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+</script>
