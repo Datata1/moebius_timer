7618859f2e2459f6acda74cd4c0dec19e88a4736
john-dev <me@slev.de>
2023-11-28 13:11:36 +0100

    Add: error page for invalid date

---
+++ pages/error/date.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="m-auto">
+    invalid date format
+    <span
+      v-if="route.query['endDate'] && route.query['endDate'] !== ''"
+      class="-ml-1"
+      >:
+      <span class="text-red-500">{{ route.query["endDate"] }}</span> (expected
+      format: YYYY-MM-DD hh:mm[:ss])</span
+    >
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+</script>
